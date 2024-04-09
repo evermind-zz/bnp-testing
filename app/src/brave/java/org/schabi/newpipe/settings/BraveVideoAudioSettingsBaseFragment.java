63ce94dc8ecb1efd522d6bdb994ef0960d618d94
evermind <evermind@tuxfamily.org>
2024-04-09 08:38:10 +0200

    BraveNewPipeLegacy: (Kitkat) make sure seekbar_preview_thumbnail_high_quality is not set
    OutOfMemoryError will be triggered as the seekbar_preview_thumbnail generation is
    allocating too much memory. This patch only allows low resolution until there
    might be a better solution

---
+++ app/src/brave/java/org/schabi/newpipe/settings/BraveVideoAudioSettingsBaseFragment.java
@@ -0,0 +1,14 @@
+package org.schabi.newpipe.settings;
+
+import android.os.Bundle;
+import android.view.View;
+
+import org.schabi.newpipe.R;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.preference.Preference;
+
+
+public abstract class BraveVideoAudioSettingsBaseFragment extends BraveBasePreferenceFragment {
+}
