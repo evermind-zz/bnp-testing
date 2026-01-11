9e906f74f55c6d8e208aa7f35e61960c6dcc78bc
evermind <evermind@tuxfamily.org>
2026-01-11 23:49:21 +0100

    BravePipe: integrate logcatviewer to show BravePipe's logcat output
    in a extra Activity or in a floating window
    settings -> bravePipe settings -> Debug Tools -> Logcat Viewer

--- app/src/main/java/org/schabi/newpipe/settings/BraveSettingsFragment.java
+++ app/src/main/java/org/schabi/newpipe/settings/BraveSettingsFragment.java
@@ -2,7 +2,10 @@ package org.schabi.newpipe.settings;
 
 import android.os.Bundle;
 
+import com.github.logviewer.LogcatActivity;
+
 import org.schabi.newpipe.App;
+import org.schabi.newpipe.R;
 
 import androidx.annotation.NonNull;
 import androidx.preference.Preference;
@@ -11,6 +14,7 @@ public class BraveSettingsFragment extends BasePreferenceFragment {
     @Override
     public void onCreatePreferences(final Bundle savedInstanceState, final String rootKey) {
         addPreferencesFromResourceRegistry();
+        setupLogcatViewer();
     }
 
     @Override
@@ -19,4 +23,18 @@ public class BraveSettingsFragment extends BasePreferenceFragment {
         return super.onPreferenceTreeClick(preference);
     }
 
+
+    private void setupLogcatViewer() {
+        final Preference pref = findPreference(requireContext().getString(
+                R.string.brave_settings_debug_logcat_viewer_key));
+        if (null != pref) {
+            pref.setOnPreferenceClickListener(preference -> {
+                        LogcatActivity.Companion.start(
+                                requireContext()
+                        );
+                        return true;
+                    }
+            );
+        }
+    }
 }
